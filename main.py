5fa02e9a2b4b26d768ee32591ffa9073f9283ecb
Gabriel <gabriel.araujo.5000@gmail.com>
2022-10-31 19:14:49 -0300

    Code to test treenode for now

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-from carpath import CarPath
-from individual import Individual
-
 import json
-from pgraph import UGraph, DGraph
+import treenode
 
-from ga import GA
+## interval_s=And(s>0, s<10)
+## conditions_s=And(signal_4[s]<1000, signal_2[s]>=-(15.27))
+## formula = (Not(ForAll([s], Implies(And(s>0, s<10), (And(signal_4[s]<1000, signal_2[s]>=-(15.27)))))))
+form = json.loads('["Not",["ForAll",[["s"],["Implies",[["And",[[">",["s",0]],["<",["s",10]]]],["And",[["<",["signal_4(s)",1000]],[">=",["signal_2(s)",-15.27]]]]]]]]]')
+# form = json.loads('ex.json')
 
-from time import sleep
+# print(len(form[1][1][1][1][1]))
+# print(    form[1][1][1][1][1][1][0][1])
 
-g = UGraph()
-# load the JSON file
-with open('routes.json', 'r') as f:
-    data = json.loads(f.read())
+t = treenode.parse(form)
 
-for name, info in data['places'].items():
-    g.add_vertex(name=name, coord=info["utm"])
-
-# create an edge for every route, and the cost is the driving distance
-for route in data['routes']:
-    g.add_edge(route['start'], route['end'], cost=route['distance'])
-
-# print(len(g._vertexlist))
-# print(len(g.edges()))
-
-ga = GA(g, 30, 3, [8, 15, 19])
-ga.evolve()
-ga.show()
-solution = ga.get_best()
-for i in range(solution.ncars):
-    g.plot(block=False)
-    g.highlight_path(solution.paths[i].path)
-
+print(t)
+print(t.count_elements())
