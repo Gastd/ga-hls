44c5d4df72b0a8f1016a0479a614fae598d6cd89
Gabriel <gabriel.araujo.5000@gmail.com>
2022-11-08 15:59:12 -0300

    Add treenode for program tree

--- treenode.py
+++ treenode.py
@@ -1,3 +1,5 @@
+import random
+from collections import deque
 
 class Node(object):
     """docstring for Node"""
@@ -16,16 +18,130 @@ class Node(object):
 
         return 1 + nel
 
+    def __repr__(self):
+        if (self.left is None) and (self.right is None):
+            return f'{repr(self.value)}'
+        else:
+            return f'[{repr(self.value)}, [{repr(self.left)},{repr(self.right)}]]'
+
+    def __str__(self):
+        if self.left is None:
+            left = ''
+        else:
+            left = str(self.left)
+
+        if self.right is None:
+            right = ''
+        else:
+            right = str(self.right)
+
+        return left + ' ' + str(self.value) + ' ' + right
+        return f'{left} {self.value} {right}'
+
+    def __len__(self):
+        return self.count_elements()
+
+    def __iadd__(self, other):
+        self.merge(other)
+
+    def get_subtree(self, node_idx: int):
+        # print(bfs(self))
+        # print(len(bfs(self)[0]))
+        return bfs(self)[0][node_idx]
+
+    def get_random_subtree(self):
+        return random.choice(bfs(self)[0][1:])
+
+    def get_parent_node(self, node):
+        queue = deque([self])
+
+        while len(queue) > 0:
+            cur_node = queue.pop()
+            # print(cur_node.value)
+            if (cur_node.left is node) or (cur_node.right is node):
+                return cur_node
+            if cur_node.left is not None:
+                queue.append(cur_node.left)
+
+            if cur_node.right is not None:
+                queue.append(cur_node.right)
+
+        return None
+
+    def cut_tree_random(self):
+        # print(self)
+        subtree = self.get_random_subtree()
+        # print(subtree)
+        childs, parents = bfs(self)
+        # print('childs = ', childs)
+        # print('parents = ', parents)
+        child_idx = childs.index(subtree)
+        parent = self.get_parent_node(subtree)
+        # print('child_idx = ', child_idx)
+        # print('len(childs) = ', len(childs))
+        # print('childs[6] = ', childs[6])
+        # print('parents[6] = ', self.get_parent_node(subtree).value)
+
+        if parent is not None:
+            if parent.left is subtree:
+                parent.left = None
+            elif parent.right is subtree:
+                parent.right = None
+            else:
+                raise ValueError("Tree cut failed")
+
+        return self, subtree, parent
+
+    def merge(self, tree):
+        if self.left is None:
+            self.left = tree
+        elif self.right is None:
+            self.right = tree
+        else:
+            raise ValueError("Tree merge failed")
+
+def bfs(root):
+    if root is None:
+        return
+    queue = deque([root])
+    vertices = []
+    parents = []
+    i = 0
+    old_node = None
+
+    while len(queue) > 0:
+        cur_node = queue.pop()
+        # print(cur_node.value)
+        if cur_node.left is not None:
+            queue.append(cur_node.left)
+
+        if cur_node.right is not None:
+            queue.append(cur_node.right)
+
+        vertices.append(cur_node)
+        parents.append(old_node)
+        old_node = cur_node
+
+    return vertices, parents
+
+def dfs(root):
+    if root is None:
+        return
+
+    dfs(root.left)
+    print(root.value, end=' ')
+    dfs(root.right)
+
 
 def parse(l: list):
     tree = None
 
-    print(f'formula: {(l)}')
+    # print(f'formula: {(l)}')
     if not isinstance(l, list):
         tree =  Node(l)
         return tree
 
-    print(f'formula length: {len(l)}')
+    # print(f'formula length: {len(l)} {l}')
     if len(l) == 2:
         if isinstance(l[0], str):
             tree = Node(l[0])
@@ -40,23 +156,34 @@ def parse(l: list):
             elif len(l[1]) == 2:
                 tree.left = parse(l[1][0])
                 tree.right = parse(l[1][1])
+    elif len(l) == 1:
+        tree = Node(l[0])
     else:
         tree = Node(l)
 
-    # if not isinstance(l[1], list):
-    #     print(f'formula: {(l[1].__class__)}')
-    #     print(f'')
-    #     tree =  Node('')
-    #     tree.left = Node(l[0])
-    #     tree.right = Node(l[1])
-    #     return tree
-
-    # if isinstance(l[0], str):
-    #     tree = Node(l[0])
-    #     if len(l[1]) == 1:
-    #         tree.left = parse(l[1][0])
-    #     elif len(l[1]) == 2:
-    #         tree.left = parse(l[1][0])
-    #         tree.right = parse(l[1][1])
-
     return tree
+
+def get_terminators(node):
+    if node.left is None and node.right is None:
+        if isinstance(node.value, list):
+            return [i for i in node.value]
+        else:
+            return [node.value]
+    else:
+        left = get_terminators(node.left)
+        right = get_terminators(node.right)
+        return left + right
+
+# A function to do inorder tree traversal
+def printInorder(root):
+ 
+    if root:
+ 
+        # First recur on left child
+        printInorder(root.left)
+ 
+        # then print the data of node
+        print(root.value),
+ 
+        # now recur on right child
+        printInorder(root.right)
