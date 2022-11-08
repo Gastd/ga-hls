a0a5330d16ef9794e7f06d3fa91d35bd25faa1ba
Gabriel <gabriel.araujo.5000@gmail.com>
2022-11-08 16:38:30 -0300

    Add tests for traversing tree

--- ga_hls/test/test_translate_prop.py
+++ ga_hls/test/test_translate_prop.py
@@ -1,16 +1,54 @@
 import pytest
 
 import json
-from ga_hls.treenode import parse
+from ga_hls.treenode import parse, get_terminators, bfs, dfs
 
 class TestProperty:
     ## interval_s=And(s>0, s<10)
     ## conditions_s=And(signal_4[s]<1000, signal_2[s]>=-(15.27))
     ## formula = (Not(ForAll([s], Implies(And(s>0, s<10), (And(signal_4[s]<1000, signal_2[s]>=-(15.27)))))))
-    def test_sigma(self):
-        form = json.loads('["Not",["ForAll",[["s"],["Implies",[["And",[[">",["s",0]],["<",["s",10]]]],["And",[["<",["signal_4(s)",1000]],[">=",["signal_2(s)",-15.27]]]]]]]]]')
+    def test_hls_v1(self):
+        form = json.loads('["ForAll",[["s"],["Implies",[["And",[[">",["s",0]],["<",["s",10]]]],["And",[["<",["signal_4(s)",1000]],[">=",["signal_2(s)",-15.27]]]]]]]]')
 
         t = parse(form)
 
-        assert t is None
-        assert t.count_elements() is None
+        assert t is not None
+        assert t.count_elements() is not None
+
+    def test_hls_v2(self):
+        form = json.loads('["ForAll",[["s"],["Implies",[["Or",[[">",["s",0]],["<",["s",10]]]],["And",[["<",["signal_4(s)",1000]],[">=",["signal_2(s)",-15.27]]]]]]]]')
+
+        t = parse(form)
+
+        assert t is not None
+        assert t.count_elements() is not None
+
+    def test_arith(self):
+        form = json.loads('[">",[["+",["x",2]],2]]')
+
+        t = parse(form)
+
+        assert t is not None
+        assert t.count_elements() is not None
+
+    def test_arith_logic(self):
+        form = json.loads('["Or",[["+",["x",10]],["<",["x",20]]]]')
+
+        t = parse(form)
+
+        assert t is not None
+        assert t.count_elements() is not None
+
+    def test_terminators(self):
+        form = json.loads('["Or",[["+",["x",10]],["<",["x",20]]]]')
+        t = get_terminators(parse(form))
+
+        assert t is not None
+
+    def test_dfs(self):
+        form = json.loads('["Or",[["+",["x",10]],["<",["x",20]]]]')
+        dfs(parse(form))
+
+    def test_bfs(self):
+        form = json.loads('["Or",[["+",["x",10]],["<",["x",20]]]]')
+        bfs(parse(form))
